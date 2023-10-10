8f0886c051827a0fd5f08a31b9e57c2bfec5e5bb
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-10-10 20:16:02 +0530

    slack notification webhook

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const { IncomingWebhook } = require('@slack/webhook');
+
+async function sendSlackMessage(message) {
+  const webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK);
+  await webhook.send(message);
+}
+
+async function main() {
+  try {
+    const jsonFilePath = process.env.JSON_FILE_PATH || 'data.json';
+    const rawData = fs.readFileSync(jsonFilePath);
+    const data = JSON.parse(rawData);
+
+    let message = '';
+
+    for (const [heading, link] of Object.entries(data)) {
+      message += `*${heading}*: ${link}\n`;
+    }
+
+    await sendSlackMessage(message);
+  } catch (error) {
+    console.error('Error:', error.message);
+    process.exit(1);
+  }
+}
+
+main();
