7753bda28f9b50fb6ab31853164e869b4cfcc007
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-10-12 00:04:27 +0530

    sending payload as axios from the index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const payloadFilePath = process.env.PAYLOAD_FILE_PATH || 'util/reusable-payload.json';
+const slackWebhookURL = process.env.SLACK_WEBHOOK_URL;
+
+const sendSlackNotification = async () => {
+    try {
+        // Read the payload from the specified file
+        const payload = JSON.parse(fs.readFileSync(payloadFilePath, 'utf-8'));
+
+        // Send the payload to Slack
+        const response = await axios.post(slackWebhookURL, payload);
+
+        console.log('Message sent to Slack:', response.data);
+    } catch (error) {
+        console.error('Error sending message to Slack:', error);
+    }
+};
+
+// Send Slack notification
+sendSlackNotification();
