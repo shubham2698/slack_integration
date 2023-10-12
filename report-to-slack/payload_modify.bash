808a1f5e42184ee4f63bd28954b96bd3d61f3ca1
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-10-12 19:11:15 +0530

    TEST AGAIN

--- report-to-slack/payload_modify.bash
+++ report-to-slack/payload_modify.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-sed -i "s|github_repo|$GITHUB_REPOSITORY|g; s|github_branch|$GITHUB_REF_NAME|g; s|test_report_link|$INPUT_TEST_REPORTS_LINKS|g; s|run_id|$GITHUB_RUN_ID|g" ./report-to-slack/reusable-payload.json
+# TEST_REPORT_LINK=$INPUT_TEST_REPORTS_LINK
+sed -i "s|github_repo|$GITHUB_REPOSITORY|g; s|github_branch|$GITHUB_REF_NAME|g; s|test_report_link|$INPUT_TEST_REPORTS_LINK|g; s|run_id|$GITHUB_RUN_ID|g" ./report-to-slack/reusable-payload.json
 # perl -pi -e 's|github_repo|$GITHUB_REPOSITORY|g; s|github_branch|$GITHUB_BRANCH|g; s|test_report_link|$TEST_REPORTS_LINK|g; s|run_id|$GITHUB_RUN_ID|g' ./report-to-slack/reusable-payload.json
