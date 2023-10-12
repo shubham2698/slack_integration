5595d093d4844ffc9f924e61abcfafaca95da2dc
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-10-12 14:50:45 +0530

    changed var syntax

--- report-to-slack/payload_modify.bash
+++ report-to-slack/payload_modify.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
-git_repo=$GITHUB_REPOSITORY
-git_branch=$GITHUB_BRANCH
-test_rep_link=$$TEST_REPORTS_LINK
-run_id=$GITHUB_RUN_ID
+git_repo=${env.GITHUB_REPOSITORY}
+git_branch=${GITHUB_BRANCH}
+test_rep_link=${TEST_REPORTS_LINK}
+run_id=${GITHUB_RUN_ID}
 sed -i "s|github_repo|$git_repo|g; s|github_branch|$git_branch|g; s|test_report_link|$test_rep_link|g; s|run_id|$run_id|g" ./report-to-slack/reusable-payload.json
 
 # perl -pi -e 's|github_repo|$GITHUB_REPOSITORY|g; s|github_branch|$GITHUB_BRANCH|g; s|test_report_link|$TEST_REPORTS_LINK|g; s|run_id|$GITHUB_RUN_ID|g' ./report-to-slack/reusable-payload.json
