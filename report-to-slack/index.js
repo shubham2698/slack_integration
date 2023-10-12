6f21b33f46b2e5f58d7a67cb792cc3b5f4854663
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-10-13 00:12:14 +0530

    changed workflow for bot

--- report-to-slack/index.js
+++ report-to-slack/index.js
@@ -1,23 +1,52 @@
-const axios = require('axios');
-const fs = require('fs');
+const core = require("@actions/core");
+const exec = require("child_process").exec;
+const axios = require("axios");
 
-const payloadFilePath = process.env.PAYLOAD_FILE_PATH || './report-to-slack/reusable-payload.json';
-const slackBotToken = process.env.SLACK_BOT_TOKEN;
-const slackChannelID = process.env.CHANNEL_ID;
+async function run() {
+  try {
+    const payloadFilePath = core.getInput("payload_file_path");
+    // const shellCommand = core.getInput("shell_command");
 
-const sendSlackNotification = async () => {
-    try {
-        // Read the payload from the specified file
-        const payload = JSON.parse(fs.readFileSync(payloadFilePath, 'utf-8'));
-        payload.channel = slackChannelID;
-        // Send the payload to Slack
-        const response = await axios.post(slackBotToken, payload);
+    // Run the shell command
+    const { stdout, stderr } = await execShellCommand(shellCommand);
 
-        console.log('Message sent to Slack:', response.data);
-    } catch (error) {
-        console.error('Error sending message to Slack:', error);
-    }
-};
+    // Send the output to Slack
+    await sendToSlack(stdout, stderr);
+  } catch (error) {
+    core.setFailed(`Action failed with error: ${error.message}`);
+  }
+}
 
-// Send Slack notification
-sendSlackNotification();
+async function execShellCommand(command) {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve({ stdout, stderr });
+      }    // const shellCommand = core.getInput("shell_command");
+    });
+  });
+}
+
+async function sendToSlack(stdout, stderr) {
+  const botToken = core.getInput("slack_bot_token");
+  const channelId = core.getInput("slack_channel_id");
+  const message = `Shell Command Output:\n\`\`\`${stdout}\`\`\`\nErrors:\n\`\`\`${stderr}\`\`\``;
+
+  try {
+    await axios.post("https://slack.com/api/chat.postMessage", {
+      channel: channelId,
+      text: message,
+    }, {
+      headers: {
+        "Authorization": `Bearer ${botToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    core.setFailed(`Failed to send message to Slack: ${error.message}`);
+  }
+}
+
+run();
