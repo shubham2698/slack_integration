6ac99c97bd906997758473000aafeaecd1d2c25b
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-10-12 15:27:48 +0530

    git changed syntax again

--- report-to-slack/payload_modify.bash
+++ report-to-slack/payload_modify.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i "s|github_repo|$INPUT_GITHUB_REPOSITORY|g; s|github_branch|$inputs.INPUT_GITHUB_BRANCH|g; s|test_report_link|$TEST_REPORTS_LINK|g; s|run_id|$GITHUB_RUN_ID|g" ./report-to-slack/reusable-payload.json
+sed -i "s|github_repo|$GITHUB_REPOSITORY|g; s|github_branch|$GITHUB_BRANCH|g; s|test_report_link|$TEST_REPORTS_LINK|g; s|run_id|$GITHUB_RUN_ID|g" ./report-to-slack/reusable-payload.json
 # perl -pi -e 's|github_repo|$GITHUB_REPOSITORY|g; s|github_branch|$GITHUB_BRANCH|g; s|test_report_link|$TEST_REPORTS_LINK|g; s|run_id|$GITHUB_RUN_ID|g' ./report-to-slack/reusable-payload.json
