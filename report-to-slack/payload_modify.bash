829930fe6b17f6367628b2883a689b83380a9f88
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-10-12 19:44:02 +0530

    test again

--- report-to-slack/payload_modify.bash
+++ report-to-slack/payload_modify.bash
@@ -3,5 +3,6 @@ GR=${ github.events.inputs.github_repository }
 GB=${ github.events.inputs.github_branch }
 GRI=${ github.events.inputs.github_run_id }
 TRL=${ github.events.inputs.test_report_link }
+echo $GR,$GB,$GRI,$TRL
 
 sed -i "s|github_repo|$GR|g; s|github_branch|$GB|g; s|test_report_link|$TRL|g; s|run_id|$GRI|g" ./report-to-slack/reusable-payload.json
