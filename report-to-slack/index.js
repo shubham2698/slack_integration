3bf2491165ac1c1f7b11c3548b8d30067bba8b8b
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-10-12 20:35:23 +0530

    TEST SLACKBOT

--- report-to-slack/index.js
+++ report-to-slack/index.js
@@ -2,15 +2,16 @@ const axios = require('axios');
 const fs = require('fs');
 
 const payloadFilePath = process.env.PAYLOAD_FILE_PATH || './report-to-slack/reusable-payload.json';
-const slackWebhookURL = process.env.SLACK_WEBHOOK_URL;
+const slackBotToken = process.env.SLACK_BOT_TOKEN;
+const slackChannelID = process.env.CHANNEL_ID;
 
 const sendSlackNotification = async () => {
     try {
         // Read the payload from the specified file
         const payload = JSON.parse(fs.readFileSync(payloadFilePath, 'utf-8'));
-
+        payload.channel = slackChannelID;
         // Send the payload to Slack
-        const response = await axios.post(slackWebhookURL, payload);
+        const response = await axios.post(slackBotToken, payload);
 
         console.log('Message sent to Slack:', response.data);
     } catch (error) {
