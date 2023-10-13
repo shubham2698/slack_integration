fab5ffe17d7e453089a9a37be21e235de01b6d59
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-10-13 14:18:18 +0530

    try-status-color

--- report-to-slack/payload_modify.bash
+++ report-to-slack/payload_modify.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i "s|github_repo|$github_repository|g; s|github_branch|$github_branch|g; s|test_report_link|$test_report_link|g; s|run_id|$github_run_id|g" ./report-to-slack/reusable-payload.json
+sed -i "s|github_repo|$github_repository|g; s|github_branch|$github_branch|g; s|test_report_link|$test_report_link|g; s|status_color|$status_color|g; s|run_id|$github_run_id|g" ./report-to-slack/reusable-payload.json
 # perl -pi -e 's|github_repo|$GITHUB_REPOSITORY|g; s|github_branch|$GITHUB_BRANCH|g; s|test_report_link|$TEST_REPORTS_LINK|g; s|run_id|$GITHUB_RUN_ID|g' ./report-to-slack/reusable-payload.json
