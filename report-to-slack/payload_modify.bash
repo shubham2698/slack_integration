4f817d8a5eeadcd71e436c634d128663f93e7d95
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-10-16 19:59:14 +0530

    sjfbhjfsdhbfds

--- report-to-slack/payload_modify.bash
+++ report-to-slack/payload_modify.bash
@@ -1,38 +1,19 @@
 #!/bin/bash
 
-# Load the JSON data from the file into a variable
-data=$(cat ./reusable-payload.json)
+# Read the JSON file and extract key-value pairs
+json_file="./report-to-slack/reusable-payload.json"
+sections=$(jq -r 'to_entries | map("\(.key): \(.value)") | .[]' "$json_file")
 
-# Start building the Slack Block Kit JSON
-blocks='{
-        "text": "sample_text",
-        "blocks": [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "GitHub Action build result: ${{ job.status }}\n${{ github.event.pull_request.html_url || github.event.head_commit.url }}"
-            }
-          }
-        ]
-      }'
+# Initialize the payload with the initial text
+payload='{"blocks":['
 
-# Iterate over the JSON data and add sections to the blocks
-while IFS="=" read -r key value; do
-  key=$key
-  value=$value
+# Construct Slack message sections dynamically
+for section in $sections; do
+  payload+='{"type":"section","text":{"type":"mrkdwn","text":"'$section'"}},'
+done
 
-  section_block='{
-    "type": "section",
-      "text":{
-        "type": "mrkdwn",
-        "text": "*'"$key"'*\n'"$value"'"
-      }
-  }'
-
-  # Add the section block to the blocks array
-  blocks=$(jq --argjson section_block "$section_block" '.blocks += [$section_block]' <<< "$blocks")
-done <<< "$(echo "$data" | jq -r 'to_entries | .[] | "\(.key) \(.value)"')"
-
-# Print the generated Slack Block Kit JSON
-echo "$blocks"
+# Remove the trailing comma and add the closing brackets for the payload
+payload=${payload%,}
+payload+=']}'
+echo "$payload" > "$json_file"
+echo "$payload"
