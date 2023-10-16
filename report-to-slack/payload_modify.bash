8ca16b91a22d0b848762ae49c52fec7b4ca1ffa2
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-10-16 13:49:54 +0530

    test dynamic payload generation shell script

--- report-to-slack/payload_modify.bash
+++ report-to-slack/payload_modify.bash
@@ -1,3 +1,42 @@
 #!/bin/bash
-sed -i "s|github_repo|$github_repository|g; s|github_branch|$github_branch|g; s|test_report_link|$test_report_link|g; s|status_color|$status_color|g; s|run_id|$github_run_id|g" ./report-to-slack/reusable-payload.json
-# perl -pi -e 's|github_repo|$GITHUB_REPOSITORY|g; s|github_branch|$GITHUB_BRANCH|g; s|test_report_link|$TEST_REPORTS_LINK|g; s|run_id|$GITHUB_RUN_ID|g' ./report-to-slack/reusable-payload.json
+
+# Load the JSON data from the file into a variable
+data=$(cat './report-to-slack/reusable-payload.json')
+
+# Start building the Slack Block Kit JSON
+blocks='[
+  {
+    "type": "header",
+    "text": {
+      "type": "plain_text",
+      "text": "Pipeline Failure Message"
+    }
+  },
+  {
+    "type": "divider"
+  }
+]'
+
+# Iterate over the JSON data and add sections to the blocks
+while IFS= read -r line; do
+  heading=$(echo "$line" | jq -r '.heading')
+  link=$(echo "$line" | jq -r '.link')
+
+  block='{
+    "type": "section",
+    "fields": [
+      {
+        "type": "mrkdwn",
+        "text": "*'$heading'*\n'$link'"
+      }
+    ]
+  }'
+
+  blocks="$blocks,$block"
+done <<< "$data"
+
+# Add closing square bracket to complete the JSON array
+blocks="$blocks]"
+
+# Print the generated Slack Block Kit JSON
+echo "$blocks"
