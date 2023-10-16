def014768bd53b2f5ea4e75a763b9a663744db75
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-10-16 18:47:56 +0530

    new payload test

--- report-to-slack/payload_modify.bash
+++ report-to-slack/payload_modify.bash
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Load the JSON data from the file into a variable
-data=$(cat ./reusable-payload.json)
+data=$(cat ./report-to-slack/reusable-payload.json)
 
 # Start building the Slack Block Kit JSON
 blocks='{
-    "text": "Pipeline Failure Details",
   "blocks": [
     {
       "type": "header",
@@ -20,22 +19,30 @@ blocks='{
   ]
 }'
 
+ 
+
 # Iterate over the JSON data and add sections to the blocks
 while IFS="=" read -r key value; do
   key=$key
   value=$value
 
   section_block='{
-    "type": "section_'"$key"'",
-      "fields":{
+    "type": "section",
+    "fields": [
+      {
         "type": "mrkdwn",
         "text": "*'"$key"'*\n'"$value"'"
       }
+    ]
   }'
+
+ 
 
   # Add the section block to the blocks array
   blocks=$(jq --argjson section_block "$section_block" '.blocks += [$section_block]' <<< "$blocks")
 done <<< "$(echo "$data" | jq -r 'to_entries | .[] | "\(.key) \(.value)"')"
+
+ 
 
 # Print the generated Slack Block Kit JSON
 echo "$blocks"
