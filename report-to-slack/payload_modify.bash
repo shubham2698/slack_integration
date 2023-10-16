363652c73e96fb24c27ef153c161db5dfbc78f70
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-10-16 19:01:25 +0530

    changed payload

--- report-to-slack/payload_modify.bash
+++ report-to-slack/payload_modify.bash
@@ -5,27 +5,28 @@ data=$(cat ./report-to-slack/reusable-payload.json)
 
 # Start building the Slack Block Kit JSON
 blocks='{
-  "blocks": [
-    {
-      "type": "header",
-      "text": {
-        "type": "plain_text",
-        "text": "Pipeline Failure"
-      }
-    },
-    {
-      "type": "divider"
-    }
-  ]
+    "text": "title_text",
+    "attachments": [
+        {
+            "color": "color_code",
+            "blocks": [
+                {
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "header_text"
+                    }
+                },
+                {
+                    "type": "divider"
+                }
+            ]
+        }
+    ]
 }'
-
- 
 
 # Iterate over the JSON data and add sections to the blocks
 while IFS="=" read -r key value; do
-  key=$key
-  value=$value
-
   section_block='{
     "type": "section",
     "fields": [
@@ -36,13 +37,9 @@ while IFS="=" read -r key value; do
     ]
   }'
 
- 
-
   # Add the section block to the blocks array
-  blocks=$(jq --argjson section_block "$section_block" '.blocks += [$section_block]' <<< "$blocks")
-done <<< "$(echo "$data" | jq -r 'to_entries | .[] | "\(.key) \(.value)"')"
-
- 
+  blocks=$(jq --argjson section_block "$section_block" '.attachments[0].blocks += [$section_block]' <<< "$blocks")
+done <<< "$(echo "$data" | jq -r 'to_entries | .[] | "\(.key)=\(.value)"')"
 
 # Print the generated Slack Block Kit JSON
 echo "$blocks"
