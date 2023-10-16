a77afd480241a7cb5e575ad8bdff14a6de472021
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-10-16 20:17:05 +0530

    last try

--- report-to-slack/payload_modify.bash
+++ report-to-slack/payload_modify.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Read the JSON file and extract key-value pairs
-json_file="./reusable-payload.json"
+json_file="./report-to-slack/reusable-payload.json"
 sections=$(jq -r 'to_entries | map("\(.key)=\(.value)") | .[]' "$json_file")
+
+# Define the while loop string
+while_loop='while IFS= read -r key value; do'
 
 # Initialize the payload with the initial text
 payload='{
