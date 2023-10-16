23bd6e91b8bde2d7446a5ca52e447d65eb28628b
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-10-16 20:03:11 +0530

    direct test

--- report-to-slack/payload_modify.bash
+++ report-to-slack/payload_modify.bash
@@ -5,15 +5,26 @@ json_file="./report-to-slack/reusable-payload.json"
 sections=$(jq -r 'to_entries | map("\(.key): \(.value)") | .[]' "$json_file")
 
 # Initialize the payload with the initial text
-payload='{"blocks":['
+payload='{
+  "text": "GitHub Action build result: ${{ job.status }}\\n${{ github.event.pull_request.html_url || github.event.head_commit.url }}",
+  "blocks": ['
 
 # Construct Slack message sections dynamically
 for section in $sections; do
-  payload+='{"type":"section","text":{"type":"mrkdwn","text":"'$section'"}},'
+  payload+='{
+    "type": "section",
+    "text": {
+      "type": "mrkdwn",
+      "text": "'"$section"'"
+    }
+  },'
 done
 
 # Remove the trailing comma and add the closing brackets for the payload
 payload=${payload%,}
 payload+=']}'
+
+# Overwrite the JSON file with the updated payload
 echo "$payload" > "$json_file"
-echo "$payload"
+
+echo "Payload updated and saved to $json_file"
