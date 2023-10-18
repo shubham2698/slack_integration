52beccc441dd8b6b91a4b078455a38ffe55b1974
Shubham Joshi <shubham_joshi@neovasolutions.in>
2023-10-18 15:11:15 +0530

    test

--- report-to-slack/payload_modify.bash
+++ report-to-slack/payload_modify.bash
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # Read the JSON file and extract key-value pairs
-echo "${{ inputs.events.payload-file-path }}"
-echo "${{ github.event.inputs.events.payload-file-path }}"
+
 echo ${INPUT_payload-file-path}
-echo ${{INPUT_payload-file-path}}
-json_file=""
+json_file="${INPUT_payload-file-path}"
 sections=$(jq -r 'to_entries | map("\(.key)=\(.value)") | .[]' "$json_file")
 
 # Define the while loop string
