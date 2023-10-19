269148b944d8bfeb617a7168f74e706cc86f1455
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-10-19 23:26:54 +0530

    test_remote_action

--- report-to-slack/payload_modify.bash
+++ report-to-slack/payload_modify.bash
@@ -1,8 +1,43 @@
 #!/bin/bash
 
+# Read the JSON file and extract key-value pairs
+print("hello world")
+json_file="$1"
+sections=$(jq -r 'to_entries | map("\(.key)=\(.value)") | .[]' "$json_file")
 
-for key in "${!env[@]}"; do
-  echo "$key=${env[$key]}"
-done
+# Define the while loop string
+while_loop='while IFS= read -r key value; do'
 
-echo "Hello"
+# Initialize the payload with the initial text
+payload='{
+    "attachments": [
+        {
+            "color": "${{ env.COLOR_CODE }}",
+            "blocks": [
+                {
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Unit Test Result ( hard-coded )"
+                    }
+                },
+                {
+                    "type": "divider"
+                }'
+
+# Construct Slack message sections dynamically
+while IFS='=' read -r key value; do
+  payload+=',{
+                "type": "section",
+                "fields": [
+                    {
+                        "type": "mrkdwn",
+                        "text": "*'$key'*\n'$value'"
+                    }
+                ]
+            }'
+done <<< "$sections"
+
+payload+=']}]}'
+
+echo "$payload" > "$json_file"
