d82c96d6e0c380554db03277cf19c3d23e4df8b5
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-10-19 23:40:29 +0530

    test_remote_action

--- report-to-slack/payload_modify.py
+++ report-to-slack/payload_modify.py
@@ -3,7 +3,40 @@ import sys
 import os
 
 print("Script Execution Started")
+
+
+payload = {
+    "attachments": [
+        {
+            "color": f"#FF0000",
+            "blocks": [
+                {
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Unit Test Result (hard-coded)"
+                    }
+                },
+                {
+                    "type": "divider"
+                }
+            ]
+        }
+    ]
+}
+
 for key, value in os.environ.items():
     if key.islower():
-        print(f"{key}={value}")
+        section = {
+            "type": "section",
+            "fields": [
+                {
+                    "type": "mrkdwn",
+                    "text": f"*{key}*\n{value}"
+                }
+            ]
+        }
+    payload["attachments"][0]["blocks"].append(section)
+
+print(payload)
 
