dc46ddb3d2dd21f57a2993ce77b9a75decec64d0
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-10-19 23:29:58 +0530

    test_remote_action

--- report-to-slack/payload_modify.py
+++ report-to-slack/payload_modify.py
@@ -2,50 +2,5 @@ import json
 import sys
 import os
 
-env_variables = os.environ.keys()
-json_data = {}
-exclude_variables = [
-    "pythonLocation",
-    "PKG_CONFIG_PATH",
-    "Python_ROOT_DIR",
-    "Python2_ROOT_DIR",
-    "Python3_ROOT_DIR",
-    "LD_LIBRARY_PATH"
-]
+print ("Hello World")
 
-
-payload = {
-    "attachments": [
-        {
-            "color": f"#FF0000",
-            "blocks": [
-                {
-                    "type": "header",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Unit Test Result (hard-coded)"
-                    }
-                },
-                {
-                    "type": "divider"
-                }
-            ]
-        }
-    ]
-}
-section = {}
-for key, value in os.environ.items():
-    if key.startswith("INPUT_") and value not in exclude_variables:
-        key = key[len("INPUT_"):]
-        section = {
-            "type": "section",
-            "fields": [
-                {
-                    "type": "mrkdwn",
-                    "text": f"*{key}*\n{value}"
-                }
-            ]
-        }
-        payload["attachments"][0]["blocks"].append(section)
-
-print(payload)
