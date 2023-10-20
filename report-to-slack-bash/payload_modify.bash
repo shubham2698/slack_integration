72b7146e320c022ee4d308af2a52e005ffc446f1
Shubham Joshi <shubhamjoshi199826@gmail.com>
2023-10-20 22:39:03 +0530

    remote_action

--- report-to-slack-bash/payload_modify.bash
+++ report-to-slack-bash/payload_modify.bash
@@ -1,46 +1,50 @@
 #!/bin/bash
 
-json_file="slack-payload-input.json"
-sections=$(jq -r 'to_entries | map("\(.key)=\(.value)") | .[]' "$json_file")
+for var in $(compgen -e); do
+    echo "$var=${!var}"
+done
 
-json_data='{}'
-output_file="payload.json"
-echo "$json_data" > "$output_file"
+# json_file="slack-payload-input.json"
+# sections=$(jq -r 'to_entries | map("\(.key)=\(.value)") | .[]' "$json_file")
 
-# Define the while loop string
-# shellcheck disable=SC2034
-while_loop='while IFS= read -r key value; do'
+# json_data='{}'
+# output_file="payload.json"
+# echo "$json_data" > "$output_file"
 
-# Initialize the payload with the initial text
-# shellcheck disable=SC2016
-payload='{
-    "fallback": "${{ env.FALLBACK }}",
-    "attachments": [
-        {
-            "color": "${{ env.COLOR_CODE }}",
-            "blocks": [
-                {
-                    "type": "header",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "${{ env.HEADER_TEXT }}"
-                    }
-                },
-                {
-                    "type": "divider"
-                }'
+# # Define the while loop string
+# # shellcheck disable=SC2034
+# while_loop='while IFS= read -r key value; do'
 
-# Construct Slack message sections dynamically
-while IFS='=' read -r key value; do
-  payload+=',{
-                "type": "section",
-                "fields": [
-                    {
-                        "type": "mrkdwn",
-                        "text": "*'$key'*\n'$value'"
-                    }
-                ]
-            }'
-done <<< "$sections"
-payload+=']}]}'
-echo "$payload" > "$output_file"
+# # Initialize the payload with the initial text
+# # shellcheck disable=SC2016
+# payload='{
+#     "fallback": "${{ env.FALLBACK }}",
+#     "attachments": [
+#         {
+#             "color": "${{ env.COLOR_CODE }}",
+#             "blocks": [
+#                 {
+#                     "type": "header",
+#                     "text": {
+#                         "type": "plain_text",
+#                         "text": "${{ env.HEADER_TEXT }}"
+#                     }
+#                 },
+#                 {
+#                     "type": "divider"
+#                 }'
+
+# # Construct Slack message sections dynamically
+# while IFS='=' read -r key value; do
+#   payload+=',{
+#                 "type": "section",
+#                 "fields": [
+#                     {
+#                         "type": "mrkdwn",
+#                         "text": "*'$key'*\n'$value'"
+#                     }
+#                 ]
+#             }'
+# done <<< "$sections"
+# payload+=']}]}'
+# echo "$payload" > "$output_file"
